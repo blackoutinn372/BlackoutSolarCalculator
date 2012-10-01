c5f81f1af25af642f2233a34e4f7c01414056a23
Court <Cour7Dawson@gmail.com>
2012-10-01 15:23:55 +1000

    Added some JDO Database support. Doesn't work, so dummied out.
    Most of the material is there, it seems usable, but needs proper
    implementation (by someone who can understand it better).

---
+++ com.blackout.solarpanelcalculator/src/tests/SolarPanelTest.java
@@ -0,0 +1,73 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.blackout.solarpanelcalculator.server.CalculationFormulas;
+import com.blackout.solarpanelcalculator.server.SolarPanel;
+@SuppressWarnings("deprecation")
+public class SolarPanelTest {
+	private static final double DELTA = 1e-10;
+	
+	SolarPanel sp;
+	@Before
+	public void SetUp() {
+		sp = new SolarPanel();
+	}
+	
+	// A default brand can be made
+	@Test
+	public void defaultBrand() {
+		assertEquals("BRAND", sp.getBrand());
+	}
+	// A default description is applies
+	@Test
+	public void defaultDescription() {
+		assertEquals("A Solar Panel", sp.getDescription());
+	}
+	// Default Power is implemented
+	@Test
+	public void defaultWatts() {
+		assertEquals(123.45, sp.getWatts(), DELTA);
+	}
+	// Default price is implemented
+	@Test
+	public void defaultPrice() {
+		assertEquals(123.45, sp.getPrice(), DELTA);
+	}
+	
+	// Can change the brand
+	@Test
+	public void changeBrand() {
+		String newBrand = "OCD";
+		sp.setBrand(newBrand);
+		assertEquals(newBrand, sp.getBrand());
+	}
+	
+	// Can change the desctiption
+	@Test
+	public void changeDescription() {
+		String newDescription = "OCD";
+		sp.setDescription(newDescription);
+		assertEquals(newDescription, sp.getDescription());
+	}
+	
+	// Can change the brand
+	@Test
+	public void changePower() {
+		double newPower = 321.01;
+		sp.setWatts(newPower);
+		assertEquals(newPower, sp.getWatts(), DELTA);
+	}
+	
+	// Can change the brand
+	@Test
+	public void changeCost() {
+		double newCost = 321.01;
+		sp.setPrice(newCost);
+		assertEquals(newCost, sp.getPrice(), DELTA);
+	}
+
+}
