36e92a9e516539cab07ba9a864b9da1bcb3171d5
Sen Li <sensmile@gmail.com>
2012-10-06 13:09:12 +1000

    updated taskallocation chart and mysqlexport file

--- com.blackout.solarpanelcalculator/docs/mysqlExportv2.sql
+++ com.blackout.solarpanelcalculator/docs/mysqlExportv2.sql
@@ -41,7 +41,7 @@ CREATE TABLE `states` (
 
 LOCK TABLES `states` WRITE;
 /*!40000 ALTER TABLE `states` DISABLE KEYS */;
-INSERT INTO `states` VALUES ('ACT','Australian Capital Territory',20,20,'2600','2920'),('NSW','New South Wales',6,22.6,'2000','2999'),('NT','Northern Territory',20,19,'0800','0899'),('QLD','Queensland',44,25,'4000','4999'),('SA','South Australia',44,23,'5000','5799'),('TAS','Tasmania',20,23,'7000','7799'),('VIC','Victoria',60,25,'3000','3999'),('WA','Western Australia',7,20,'6000','6797');
+INSERT INTO `states` VALUES ('ACT','Australian Capital Territory',20,20,'2600','2920'),('NSW','New South Wales',6,22.6,'2000','2599'),('NT','Northern Territory',20,19,'0800','0899'),('QLD','Queensland',44,25,'4000','4999'),('SA','South Australia',44,23,'5000','5799'),('TAS','Tasmania',20,23,'7000','7799'),('VIC','Victoria',60,25,'3000','3999'),('WA','Western Australia',7,20,'6000','6797');
 /*!40000 ALTER TABLE `states` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +56,7 @@ CREATE TABLE `cities` (
   `CityID` int(11) NOT NULL,
   `CityName` varchar(18) NOT NULL,
   `Postcode` char(6) NOT NULL,
-  `ZoneRating` double DEFAULT NULL,
+  `ZoneRating` double DEFAULT '0',
   `CapitalCity` char(6) NOT NULL DEFAULT 'False',
   `StateCode` char(6) NOT NULL,
   `OptimalYearRound` int(11) NOT NULL,
@@ -74,7 +74,7 @@ CREATE TABLE `cities` (
   `Oct` double NOT NULL,
   `Nov` double NOT NULL,
   `Dec` double NOT NULL,
-  `AvgProducePerkw` double DEFAULT NULL,
+  `AvgProducePerkw` double DEFAULT '0',
   PRIMARY KEY (`CityID`),
   KEY `StateCode` (`StateCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -86,7 +86,7 @@ CREATE TABLE `cities` (
 
 LOCK TABLES `cities` WRITE;
 /*!40000 ALTER TABLE `cities` DISABLE KEYS */;
-INSERT INTO `cities` VALUES (1,'Sydney','2000',1.382,'True','NSW',56,41,71,5.91,5.25,4.48,3.56,2.73,2.49,2.68,3.49,4.6,5.41,5.88,6.24,3.9),(2,'Newcastle','2300',NULL,'false','NSW',57,42,72,6.21,5.47,4.69,3.77,2.87,2.62,2.82,3.66,4.37,5.63,6.08,6.49,NULL),(3,'Wollongong','2500',NULL,'false','NSW',56,41,71,6.19,5.44,4.64,3.53,2.68,2.34,2.58,3.34,4.48,5.42,6,6.44,NULL),(4,'Canberra','2600',1.382,'True','ACT',55,40,70,6.78,6.01,5,3.68,2.68,2.23,2.45,3.19,4.37,5.48,6.36,6.86,4.3),(5,'Melbourne','3000',1.185,'True','VIC',52,37,67,5.91,5.83,4.51,3.23,2.23,1.78,1.94,2.59,3.55,4.72,5.74,6.22,3.6),(6,'Geelong','3200',NULL,'false','VIC',52,37,67,6.41,5.78,4.44,3.12,2.17,1.81,1.99,2.68,3.78,5.03,6.03,6.39,NULL),(7,'Brisbane','4000',1.382,'True','QLD',58,43,73,6.19,5.39,4.95,3.98,3.23,3.02,3.22,4.04,5.12,5.52,6.07,6.35,4.2),(8,'Gold Coast','4217',NULL,'false','QLD',62,47,77,6.19,5.44,4.95,3.98,3.27,3.03,3.22,4.04,4.77,5.52,6.07,6.35,NULL),(9,'Toowoomba','4350',NULL,'false','QLD',62,47,77,6.69,5.8,5.31,4.31,3.49,3.26,3.47,4.38,5.52,5.99,6.59,6.82,NULL),(10,'Townsville','4810',NULL,'false','QLD',71,56,86,6.35,5.56,5.56,4.79,4.27,4,4.39,5.13,6.18,6.59,6.62,6.71,NULL),(11,'Cairns','4870',NULL,'false','QLD',73,58,88,5.83,5.18,5.07,4.81,4.35,4.24,4.47,5.07,5.98,6.59,6.5,6.27,4.2),(12,'Adelaide','5000',1.382,'True','SA',55,40,70,8.41,7.49,5.93,4.34,3.09,2.62,2.82,3.62,5.12,6.41,7.71,8.45,4.2),(13,'Perth','6000',1.382,'True','WA',58,43,73,8.41,7.49,5.93,4.34,3.09,2.62,2.82,3.62,5.04,6.41,7.71,8.45,4.4),(14,'Hobart','7000',1.185,'True','TAS',47,32,62,5.78,5.18,3.92,2.61,1.8,1.46,1.62,2.33,3.39,4.62,5.52,6,3.5),(15,'Darwin','0800',1.536,'True','NT',78,78,93,5.52,5.2,5.9,5.99,5.82,5.55,5.76,6.36,6.87,7.07,6.79,5.84,4.4),(16,'Alice Springs','0870',NULL,'False','NT',66,51,81,7.5,6.78,6.33,5.47,4.33,3.98,4.35,5.27,6.17,6.77,7.26,7.37,5),(17,'Burnie','7320',NULL,'False','TAS',49,34,64,5.8,5.25,3.91,2.54,1.73,1.38,1.54,2.16,3.17,4.36,5.49,5.99,NULL),(18,'Bendigo','3550',NULL,'False','VIC',53,38,68,7.07,6344,5.18,3.77,2.55,2.01,2.18,2.92,4.05,5.3,6.49,6.96,NULL),(19,'Albany','6330',NULL,'False','WA',55,40,70,6.88,6.16,4.71,3.51,2.52,2.2,2.36,2.93,4.01,5.04,6.07,6.81,NULL),(20,'Mount Gambier','5290',NULL,'False','SA',52,37,67,6.55,5.92,4.58,3.22,2.27,1.87,2.08,2.77,3.84,5.07,6.11,6.43,NULL);
+INSERT INTO `cities` VALUES (0,'Sydney','2000',1.382,'True','NSW',56,41,71,5.91,5.25,4.48,3.56,2.73,2.49,2.68,3.49,4.6,5.41,5.88,6.24,3.9),(1,'Newcastle','2300',0,'false','NSW',57,42,72,6.21,5.47,4.69,3.77,2.87,2.62,2.82,3.66,4.37,5.63,6.08,6.49,0),(2,'Wollongong','2500',0,'false','NSW',56,41,71,6.19,5.44,4.64,3.53,2.68,2.34,2.58,3.34,4.48,5.42,6,6.44,0),(3,'Canberra','2600',1.382,'True','ACT',55,40,70,6.78,6.01,5,3.68,2.68,2.23,2.45,3.19,4.37,5.48,6.36,6.86,4.3),(4,'Melbourne','3000',1.185,'True','VIC',52,37,67,5.91,5.83,4.51,3.23,2.23,1.78,1.94,2.59,3.55,4.72,5.74,6.22,3.6),(5,'Geelong','3200',0,'false','VIC',52,37,67,6.41,5.78,4.44,3.12,2.17,1.81,1.99,2.68,3.78,5.03,6.03,6.39,0),(6,'Brisbane','4000',1.382,'True','QLD',58,43,73,6.19,5.39,4.95,3.98,3.23,3.02,3.22,4.04,5.12,5.52,6.07,6.35,4.2),(7,'Gold Coast','4217',0,'false','QLD',62,47,77,6.19,5.44,4.95,3.98,3.27,3.03,3.22,4.04,4.77,5.52,6.07,6.35,0),(8,'Toowoomba','4350',0,'false','QLD',62,47,77,6.69,5.8,5.31,4.31,3.49,3.26,3.47,4.38,5.52,5.99,6.59,6.82,0),(9,'Townsville','4810',0,'false','QLD',71,56,86,6.35,5.56,5.56,4.79,4.27,4,4.39,5.13,6.18,6.59,6.62,6.71,0),(10,'Cairns','4870',0,'false','QLD',73,58,88,5.83,5.18,5.07,4.81,4.35,4.24,4.47,5.07,5.98,6.59,6.5,6.27,4.2),(11,'Adelaide','5000',1.382,'True','SA',55,40,70,8.41,7.49,5.93,4.34,3.09,2.62,2.82,3.62,5.12,6.41,7.71,8.45,4.2),(12,'Perth','6000',1.382,'True','WA',58,43,73,8.41,7.49,5.93,4.34,3.09,2.62,2.82,3.62,5.04,6.41,7.71,8.45,4.4),(13,'Hobart','7000',1.185,'True','TAS',47,32,62,5.78,5.18,3.92,2.61,1.8,1.46,1.62,2.33,3.39,4.62,5.52,6,3.5),(14,'Darwin','0800',1.536,'True','NT',78,78,93,5.52,5.2,5.9,5.99,5.82,5.55,5.76,6.36,6.87,7.07,6.79,5.84,4.4),(15,'Alice Springs','0870',0,'False','NT',66,51,81,7.5,6.78,6.33,5.47,4.33,3.98,4.35,5.27,6.17,6.77,7.26,7.37,5),(16,'Burnie','7320',0,'False','TAS',49,34,64,5.8,5.25,3.91,2.54,1.73,1.38,1.54,2.16,3.17,4.36,5.49,5.99,0),(17,'Bendigo','3550',0,'False','VIC',53,38,68,7.07,6.3,5.18,3.77,2.55,2.01,2.18,2.92,4.05,5.3,6.49,6.96,0),(18,'Albany','6330',0,'False','WA',55,40,70,6.88,6.16,4.71,3.51,2.52,2.2,2.36,2.93,4.01,5.04,6.07,6.81,0),(19,'Mount Gambier','5290',0,'False','SA',52,37,67,6.55,5.92,4.58,3.22,2.27,1.87,2.08,2.77,3.84,5.07,6.11,6.43,0);
 /*!40000 ALTER TABLE `cities` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +106,7 @@ CREATE TABLE `solarproducts` (
   `Efficiency` double DEFAULT NULL,
   `Price` double NOT NULL,
   PRIMARY KEY (`ProductID`)
-) ENGINE=InnoDB AUTO_INCREMENT=544 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,7 +115,7 @@ CREATE TABLE `solarproducts` (
 
 LOCK TABLES `solarproducts` WRITE;
 /*!40000 ALTER TABLE `solarproducts` DISABLE KEYS */;
-INSERT INTO `solarproducts` VALUES (12,'Inverters','Aurora','PVI-4.2-AU Outdoor Grid Connect  IP65 rated ',4200,0.96,2376),(20,'Inverters','Aurora','PVI-3.6-AU Outdoor Grid Connect IIP65 rated ',3600,0.97,1980),(21,'Solar panels','SolarOne',' 24V, 5 inch cell Monocrystalline Cell Solar Module',195,0.1512,418),(23,'Inverters','Fronius','IG TL 3.0 Indoor and Outdoor - ',3313,0.98,2046),(29,'Inverters','Aurora','PVI-2000-AU Outdoor Grid Connect  IP65 rated',2000,0.96,1174),(32,'Solar panels','REC Solar',' Peak Energy Series, polycrystalline cell Black Frame',250,0.151,412),(43,'Inverters','Fronius','IG15 Outdoor -  Grid Connect ',1500,0.94,1575),(44,'Solar panels','Sharp',' 12V Polycrystalline',130,0.1599,554),(45,'Solar panels','Panasonic',' HIT Solar Module',235,0.186,882),(543,'Solar panels','Suntech','12V Polycrystalline ',140,0.151,421);
+INSERT INTO `solarproducts` VALUES (0,'Inverters','Aurora','PVI-4.2-AU Outdoor Grid Connect  IP65 rated ',4200,0.96,2376),(1,'Inverters','Aurora','PVI-3.6-AU Outdoor Grid Connect IIP65 rated ',3600,0.97,1980),(2,'Solar panels','SolarOne',' 24V, 5 inch cell Monocrystalline Cell Solar Module',195,0.1512,418),(3,'Inverters','Fronius','IG TL 3.0 Indoor and Outdoor - ',3313,0.98,2046),(4,'Inverters','Aurora','PVI-2000-AU Outdoor Grid Connect  IP65 rated',2000,0.96,1174),(5,'Solar panels','REC Solar',' Peak Energy Series, polycrystalline cell Black Frame',250,0.151,412),(6,'Inverters','Fronius','IG15 Outdoor -  Grid Connect ',1500,0.94,1575),(7,'Solar panels','Sharp',' 12V Polycrystalline',130,0.1599,554),(8,'Solar panels','Panasonic',' HIT Solar Module',235,0.186,882),(9,'Solar panels','Suntech','12V Polycrystalline ',140,0.151,421);
 /*!40000 ALTER TABLE `solarproducts` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -128,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-10-03 18:31:55
+-- Dump completed on 2012-10-06 13:06:27
